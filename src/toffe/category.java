39cb4d6dec6eedd3a33592ab1f321f2a09af69b6
Salma <104686897+salmaaosmann@users.noreply.github.com>
2023-05-14 05:31:29 +0300

    Documentation for category.java

--- src/toffe/category.java
+++ src/toffe/category.java
@@ -1,3 +1,12 @@
+/*Category class holds all the item assigned to it
+ * Attributes:
+ * 			name - the name of the category set with the constructor call
+ * 			itemList - a list of all the items available in this list
+ *Functions:
+ *			Category - constructor takes a name and the catalog (we only have one unique catalog in this system)
+ *			viewAllCategoryItems - views all the items stored in the list of this Category
+ * 			getName - gets the name of this category
+ */
 package toffe;
 
 import java.util.ArrayList;
